99cddf1fba1ebb40d69795d03ea2340d74bd4a70
flyingcat1998 <flyingcat1998@gmail.com>
2019-11-20 20:59:41 +0700

    Change to apk file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.download(path.join(__dirname, 'docs/doc1.txt'), function (err) {
+  res.download(path.join(__dirname, 'docs/SoundMeasure.apk'), function (err) {
 
     console.log(err);
   });
